5b7d3d83197c7d2d27db121e06401c089772cabf
Brandon Lucia <blucia@gmail.com>
2019-12-31 17:17:38 -0500

    added some basic datastructure manipulation code and made random work

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import init from "./pkg/hello_world.js";
+import init from "./pkg/rustwasm_hello_world.js";
 
 const runWasm = async () => {
-  const hello = await init("./pkg/hello_world_bg.wasm");
+  const hello = await init("./pkg/rustwasm_hello_world_bg.wasm");
   const addResult = hello.add(10,20);
-  document.body.textContent = `Hello! ${addResult}`;
+  document.body.textContent = `Hello!!!! ${addResult}`;
+
+  hello.setup_stars(10);  
 };
 
 runWasm();
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,72 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32{
+extern crate rand;
+use rand::Rng;
 
-  return a + b;
+
+#[wasm_bindgen]
+extern "C" {
+  // Use `js_namespace` here to bind `console.log(..)` instead of just
+  // `log(..)`
+#[wasm_bindgen(js_namespace = console)]
+  fn log(s: &str);
+}
+
+const XMAX: u64 = 1024;
+const YMAX: u64 = 1024;
+const VMAX: u64 = 10;
+const RMAX: u64 = 5;
+
+#[derive(Debug)]
+struct Coord {
+  x: u64,
+  y: u64
+}
+
+#[derive(Debug)]
+struct Star {
+  position: Coord,
+  velocity: Coord,
+  radius: u64
+}
+
+impl ToString for Star {
+  fn to_string(&self) -> String{
+    format!("{:?}",self)
+  }
+}
+
+fn print_star(s: &Star){
+  log(&s.to_string()[0..]); 
+}
+
+#[wasm_bindgen]
+pub fn setup_stars(num_stars: usize) {
+
+  let mut rng = rand::thread_rng();
+
+  let mut stars = Vec::new();
+  for _ in 0..num_stars{
+    stars.push( Star{ 
+                      position: 
+                               Coord { x: rng.gen_range(0,XMAX), 
+                                       y: rng.gen_range(0,YMAX) }, 
+                      velocity: 
+                               Coord { x: rng.gen_range(0,VMAX),
+                                       y: rng.gen_range(0,VMAX)},
+                      radius: rng.gen_range(0,RMAX)} );
+  }
+  log("GOT HERE!!\n");  
+  for s in &stars{
+    print_star(s);
+  }
+
+  //stars;
 
 }
 
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32{
+  return a + b;
+}
 
