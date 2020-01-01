2e5c01f32995ccddbb7b5ed836b39db7dbecb09e
Brandon Lucia <blucia@gmail.com>
2020-01-01 15:54:10 -0500

    figured out data marshaling for non-byte values and fixed a few small bugs

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 import init from "./pkg/rustwasm_hello_world.js";
 
 const runWasm = async () => {
-  const hello = await init("./pkg/rustwasm_hello_world_bg.wasm");
-  const addResult = hello.add(10,20);
-  document.body.textContent = `Hello!!!! ${addResult}`;
 
-  hello.setup_stars(10);  
+  const rustWasm = await init("./pkg/rustwasm_hello_world_bg.wasm");
+  rustWasm.setup_stars();  
+
+  const drawStarField = () => {
+    var c = document.getElementById("starCanvas");
+    var ctx = c.getContext("2d");
+  
+    const starsPtr = rustWasm.get_star_buf_ptr();
+    const numStars = rustWasm.get_num_stars(); 
+    const starSize = rustWasm.get_star_size(); 
+
+    rustWasm.update_stars();
+
+    const wasmMem = new Uint8Array(rustWasm.memory.buffer);
+    const starData = wasmMem.slice(starsPtr,starsPtr + (numStars * starSize * 4));
+  
+    ctx.clearRect(0,0,c.height,c.width);
+    for(var i = 0; i < numStars * starSize * 4; i += starSize*4){
+      var x = starData[i + 3] << 24 | starData[i+2] << 16 | starData[i+1] << 8 | starData[i];
+      var y = starData[i + 7] << 24 | starData[i+6] << 16 | starData[i+5] << 8 | starData[i+4];
+      var r = starData[i + 19] << 24 | starData[i + 18] << 16 | starData[i + 17] << 8 | starData[i + 16];
+        //console.log("[star "+x+" "+y+" "+r+"]");
+      ctx.beginPath();
+      ctx.rect(x, y, r, r);
+      ctx.stroke();
+    }
+  };
+
+  drawStarField();
+  setInterval(() => {
+    drawStarField();
+  }, 30); 
+
 };
 
 runWasm();
--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,38 @@ use rand::Rng;
 
 #[wasm_bindgen]
 extern "C" {
-  // Use `js_namespace` here to bind `console.log(..)` instead of just
-  // `log(..)`
+
 #[wasm_bindgen(js_namespace = console)]
   fn log(s: &str);
 }
 
-const XMAX: u64 = 1024;
-const YMAX: u64 = 1024;
-const VMAX: u64 = 10;
-const RMAX: u64 = 5;
+const XMAX: u32 = 1024;
+const YMAX: u32 = 1024;
+const VMAX: u32 = 5;
+const RMAX: u32 = 6;
 const NUM_STARS: usize = 256;
 
+//pos x,y  vel x,y  rad --> 5 values
 const STAR_SZ: usize = 5;
-const STAR_BUF_SZ: usize = NUM_STARS * STAR_SZ; //pos x,y  vel x,y  rad --> 5 values
-static mut STAR_BUF: [u64;  STAR_BUF_SZ] = [0; STAR_BUF_SZ];
+
+const STAR_BUF_SZ: usize = NUM_STARS * STAR_SZ; 
+
+/*WASM linear memory buffer*/
+static mut STAR_BUF: [u32;  STAR_BUF_SZ] = [0; STAR_BUF_SZ];
 
 #[wasm_bindgen]
-pub fn get_star_buf_ptr() -> *const u64 {
-  let pointer: *const u64;
+pub fn get_num_stars() -> usize{
+  NUM_STARS 
+}
+
+#[wasm_bindgen]
+pub fn get_star_size() -> usize {
+  STAR_SZ
+}
+
+#[wasm_bindgen]
+pub fn get_star_buf_ptr() -> *const u32 {
+  let pointer: *const u32;
   unsafe {
     pointer = STAR_BUF.as_ptr();
   }
@@ -34,7 +47,8 @@ pub fn get_star_buf_ptr() -> *const u64 {
 
 pub fn get_star(ind: usize) -> Option<Star>{
   if ind > NUM_STARS{ return None; }
-  let mut s: Star = Star { position: Coord{x:0,y:0}, velocity: Coord{x:0,y:0}, radius: 0};
+  let mut s: Star = 
+    Star { position: Coord{x:0,y:0}, velocity: Coord{x:0,y:0}, radius: 0};
   unsafe {
     let px = STAR_BUF[ind * STAR_SZ];
     let py = STAR_BUF[ind * STAR_SZ + 1];
@@ -44,15 +58,16 @@ pub fn get_star(ind: usize) -> Option<Star>{
     s.position.x = px;
     s.position.y = py;
     s.velocity.x = vx;
-    s.velocity.x = vy;
+    s.velocity.y = vy;
     s.radius = ra;
   }
   return Some(s); 
   
 }
 
-pub fn put_star(ind: usize, s: Star) {
+pub fn put_star(ind: usize, s: &Star) {
   if ind > NUM_STARS{ return; }
+  //log(&format!("{}",s.position.x)[0..]);
   unsafe {
     STAR_BUF[ind * STAR_SZ] = s.position.x;
     STAR_BUF[ind * STAR_SZ + 1] = s.position.y;
@@ -65,15 +80,15 @@ pub fn put_star(ind: usize, s: Star) {
 
 #[derive(Debug)]
 pub struct Coord {
-  x: u64,
-  y: u64
+  x: u32,
+  y: u32
 }
 
 #[derive(Debug)]
 pub struct Star {
   position: Coord,
   velocity: Coord,
-  radius: u64
+  radius: u32
 }
 
 impl ToString for Star {
@@ -82,7 +97,7 @@ impl ToString for Star {
   }
 }
 
-fn print_star(s: Star){
+fn print_star(s: &Star){
   log(&s.to_string()[0..]); 
 }
 
@@ -92,22 +107,49 @@ pub fn setup_stars() {
   let mut rng = rand::thread_rng();
 
   for i in 0..NUM_STARS{
-      put_star(i, Star{ position: 
-                                 Coord { x: rng.gen_range(0,XMAX), 
-                                         y: rng.gen_range(0,YMAX) }, 
+      let x = rng.gen_range(1,XMAX);
+      let y = rng.gen_range(1,YMAX);
+      let vx = rng.gen_range(1,VMAX);
+      let vy = rng.gen_range(1,VMAX);
+      let r = rng.gen_range(1,RMAX);
+      put_star(i, &Star{ position: 
+                         Coord { x: x, 
+                                 y: y}, 
                         velocity: 
-                                 Coord { x: rng.gen_range(0,VMAX),
-                                         y: rng.gen_range(0,VMAX)},
-                        radius: rng.gen_range(0,RMAX)} );
+                         Coord { x: vx,
+                                 y: vy},
+                        radius: r} );
   }
-  log("GOT HERE!!\n");  
+
+  for i in 0..NUM_STARS {
+
+    let os = get_star(i);
+    match os{
+      Some(s) => print_star(&s),
+      None => (),
+    } 
+
+  }
+
+}
+
+#[wasm_bindgen]
+pub fn update_stars() {
+
   for i in 0..NUM_STARS {
     let os = get_star(i);
     match os{
-      Some(s) => print_star(s),
+      Some(s) => { 
+                       put_star(i,&Star{ 
+                                    position: 
+                                     Coord{ x: s.position.x + s.velocity.x, 
+                                            y: s.position.y + s.velocity.y}, 
+                                    velocity: s.velocity, 
+                                    radius: s.radius}); 
+                 },
       None => (),
-    } 
-  }
+    }          
+  } 
 
 }
 
